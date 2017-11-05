106e7d070b34aa858329a4bfc16ff9ccd24be607
Sudar Muthu <sudar@sudarmuthu.com>
2017-11-05 20:24:54 +0530

    Mention that ajaxurl is a global variable

--- js/bulk-move.js
+++ js/bulk-move.js
@@ -4,11 +4,10 @@
  * http://sudarmuthu.com/wordpress/bulk-move
  *
  * @author: Sudar <http://sudarmuthu.com>
- * 
  */
 
 /*jslint browser: true, devel: true*/
-/*global BULK_MOVE, jQuery, document, postboxes, pagenow*/
+/*global BULK_MOVE, jQuery, document, postboxes, pagenow, ajaxurl*/
 jQuery(document).ready(function () {
     jQuery('button[value="bulk-move-tags"], button[value="bulk-move-cats"], button[value="bulk-move-category-by-tag"]').click(function () {
         return confirm(BULK_MOVE.msg.move_warning);
@@ -82,5 +81,4 @@ jQuery(document).ready(function () {
 			});
 		}
 	});
-
 });
