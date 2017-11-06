19f84e623020c49a4ca4b5214150be603c74d06d
Sudar Muthu <sudar@sudarmuthu.com>
2017-11-06 16:41:32 +0530

    Show warning before bulk moving posts

--- assets/js/bulk-move.js
+++ assets/js/bulk-move.js
@@ -9,8 +9,8 @@
 /*jslint browser: true, devel: true*/
 /*global BULK_MOVE, jQuery, document, postboxes, pagenow, ajaxurl*/
 jQuery(document).ready(function () {
-    jQuery('button[value="bulk-move-tags"], button[value="bulk-move-cats"], button[value="bulk-move-category-by-tag"]').click(function () {
-        return confirm(BULK_MOVE.msg.move_warning);
+    jQuery( 'button[value="move_tags"], button[value="move_cats"], button[value="move_category_by_tag"], button[value="move_custom_taxonomy"]' ).click( function () {
+        return confirm( BULK_MOVE.msg.move_warning );
     });
 
     // for post boxes
