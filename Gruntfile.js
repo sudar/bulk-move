c4c208a92733c95d828d126a9280076ec8e26a3e
Sudar Muthu <sudar@sudarmuthu.com>
2017-11-08 19:20:50 +0530

    Add build command to create build zip file

---
+++ Gruntfile.js
@@ -0,0 +1,65 @@
+/* global module, require */
+module.exports = function( grunt ) {
+	'use strict';
+
+	// Load all grunt tasks
+	require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+	// Project configuration
+	grunt.initConfig( {
+		pkg:    grunt.file.readJSON( 'package.json' ),
+
+		clean   : {
+			dist: ['dist/']
+		},
+
+		copy: {
+			dist: {
+				files : [
+					{
+						expand: true,
+						src: [
+							'**',
+							'!dist/**',
+							'!AUTHORS.md',
+							'!assets-wp-repo/**',
+							'!code-coverage/**',
+							'!codeception.yml',
+							'!node_modules/**',
+							'!assets/vendor/**',
+							'!assets/js/src/**',
+							'!assets/css/src/**',
+							'!Gruntfile.js',
+							'!bower.json',
+							'!package.json',
+							'!package-lock.json',
+							'!composer.json',
+							'!composer.lock',
+							'!phpcs.xml',
+							'!phpdoc.dist.xml',
+							'!phpunit.xml.dist',
+							'!bin/**',
+							'!tests/**',
+							'!.idea/**',
+							'!tags',
+							'!vendor/**'
+						],
+						dest: 'dist/'
+					}
+				]
+			}
+		},
+		watch: {
+			all: {
+				files: ['**', '!dist/**'],
+				tasks: ['build']
+			}
+		}
+	} );
+
+	require('time-grunt')(grunt);
+
+	grunt.registerTask('build', ['clean', 'copy:dist']);
+
+	grunt.util.linefeed = '\n';
+};
