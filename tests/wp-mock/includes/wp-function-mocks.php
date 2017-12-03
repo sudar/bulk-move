3ca51fdb119fa47e69166e9ac98165c219884849
Sudar Muthu <sudar@sudarmuthu.com>
2017-12-03 22:53:01 +0530

    Setup WP Mock tests

---
+++ tests/wp-mock/includes/wp-function-mocks.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ *
+ */
+
+if ( ! function_exists( 'plugin_basename' ) ) {
+	function plugin_basename() {
+		return \WP_Mock\Handler::predefined_return_function_helper( __FUNCTION__, func_get_args() );
+	}
+}
+
+if ( ! function_exists( 'plugin_dir_url' ) ) {
+	function plugin_dir_url() {
+		return \WP_Mock\Handler::predefined_return_function_helper( __FUNCTION__, func_get_args() );
+	}
+}
+
+if ( ! function_exists( 'load_plugin_textdomain' ) ) {
+	function load_plugin_textdomain() {
+		return \WP_Mock\Handler::predefined_return_function_helper( __FUNCTION__, func_get_args() );
+	}
+}
+
+if ( ! function_exists( 'plugin_dir_path' ) ) {
+	/**
+	 * Get the filesystem directory path (with trailing slash) for the plugin __FILE__ passed in.
+	 *
+	 * @since 2.8.0
+	 *
+	 * @param string $file The filename of the plugin (__FILE__).
+	 *
+	 * @return string the filesystem path of the directory that contains the plugin.
+	 */
+	function plugin_dir_path( $file ) {
+		return trailingslashit( dirname( $file ) );
+	}
+}
+
+if ( ! function_exists( 'trailingslashit' ) ) {
+	/**
+	 * Appends a trailing slash.
+	 *
+	 * Will remove trailing forward and backslashes if it exists already before adding
+	 * a trailing forward slash. This prevents double slashing a string or path.
+	 *
+	 * The primary use of this is for paths and thus should be used for paths. It is
+	 * not restricted to paths and offers no specific path support.
+	 *
+	 * @since 1.2.0
+	 *
+	 * @param string $string What to add the trailing slash to.
+	 *
+	 * @return string String with trailing slash added.
+	 */
+	function trailingslashit( $string ) {
+		return untrailingslashit( $string ) . '/';
+	}
+}
+
+if ( ! function_exists( 'untrailingslashit' ) ) {
+	/**
+	 * Removes trailing forward slashes and backslashes if they exist.
+	 *
+	 * The primary use of this is for paths and thus should be used for paths. It is
+	 * not restricted to paths and offers no specific path support.
+	 *
+	 * @since 2.2.0
+	 *
+	 * @param string $string What to remove the trailing slashes from.
+	 *
+	 * @return string String without the trailing slashes.
+	 */
+	function untrailingslashit( $string ) {
+		return rtrim( $string, '/\\' );
+	}
+}
