6fc1e03200978efab438d8a65af3965a0922be47
Sudar Muthu <sudar@sudarmuthu.com>
2017-12-03 22:52:27 +0530

    Move WordPress core unit tests into wp-tests directory

--- tests/wp-tests/bootstrap.php
+++ tests/wp-tests/bootstrap.php
@@ -21,7 +21,7 @@ require_once $_tests_dir . '/includes/functions.php';
  * Manually load the plugin being tested.
  */
 function _manually_load_plugin() {
-	require dirname( dirname( __FILE__ ) ) . '/bulk-move.php';
+	require dirname( dirname( __FILE__ ) ) . '/../bulk-move.php';
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
