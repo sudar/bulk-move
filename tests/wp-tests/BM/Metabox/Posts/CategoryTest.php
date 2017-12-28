ec6ebe97c747b285fda643d60918c77ca8f9cb62
anandB000 <anandrajbalu00@gmail.com>
2017-12-28 21:23:23 +0530

    Add basic test for default category
    Add basic test for default category and new branch '38-fix/add-test-for-default-category' created
    
    see #38

--- tests/wp-tests/BM/Metabox/Posts/CategoryTest.php
+++ tests/wp-tests/BM/Metabox/Posts/CategoryTest.php
@@ -265,4 +265,40 @@ class CategoryTest extends \BM_TestCase {
 		$posts_in_cat2 = $this->get_posts_by_category( $cat2 );
 		$this->assertEquals( count( $posts_in_cat2 ), 1 );
 	}
+	
+	/**
+	 * Test basic case of moving default category.
+	 */
+	public function test_move_posts_from_default_cat_to_another() {
+		// Create one categories and get default category.
+		$cat1 = get_option( 'default_category' );
+		$cat2 = $this->factory->category->create( array( 'name' => 'cat2' ) );
+
+		// Create one post in each category.
+		$post1 = $this->factory->post->create( array( 'post_title' => 'post1', 'post_category' => array( $cat1 ) ) );
+		$post2 = $this->factory->post->create( array( 'post_title' => 'post2', 'post_category' => array( $cat2 ) ) );
+
+		// Assert that each category has one post.
+		$posts_in_cat1 = $this->get_posts_by_category( $cat1 );
+		$posts_in_cat2 = $this->get_posts_by_category( $cat2 );
+
+		$this->assertEquals( count( $posts_in_cat1 ), 1 );
+		$this->assertEquals( count( $posts_in_cat2 ), 1 );
+
+		// call our method.
+		$options = array(
+			'old_cat'   => $cat1,
+			'new_cat'   => $cat2,
+			'overwrite' => true,
+		);
+		$this->category_metabox->move( $options );
+
+		// Assert that category 1 (or) default category has no posts.
+		$posts_in_cat1 = $this->get_posts_by_category( $cat1 );
+		$this->assertEquals( count( $posts_in_cat1 ), 0 );
+
+		// Assert that category 2 has two posts.
+		$posts_in_cat2 = $this->get_posts_by_category( $cat2 );
+		$this->assertEquals( count( $posts_in_cat2 ), 2 );
+	}
 }
