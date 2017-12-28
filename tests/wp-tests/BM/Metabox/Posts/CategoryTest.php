a52092800a57939495d7ac6b83bfab95dc77be5d
anandB000 <anandrajbalu00@gmail.com>
2017-12-28 21:47:50 +0530

    Add test for default category with overwrite option
    see #38

--- tests/wp-tests/BM/Metabox/Posts/CategoryTest.php
+++ tests/wp-tests/BM/Metabox/Posts/CategoryTest.php
@@ -301,4 +301,48 @@ class CategoryTest extends \BM_TestCase {
 		$posts_in_cat2 = $this->get_posts_by_category( $cat2 );
 		$this->assertEquals( count( $posts_in_cat2 ), 2 );
 	}
+	
+	/**
+	 * Test basic case of moving default category with overwrite.
+	 */
+	public function test_move_posts_from_default_cat_to_another_with_overwrite() {
+		// Create two categories and get default category.
+		$cat1 = get_option( 'default_category' );
+		$cat2 = $this->factory->category->create( array( 'name' => 'cat2' ) );
+		$common_cat = $this->factory->category->create( array( 'name' => 'common_cat' ) );
+
+		// Create one post in each category.
+		// The first post will also have the common category.
+		$post1 = $this->factory->post->create( array( 'post_title' => 'post1', 'post_category' => array( $cat1, $common_cat ) ) );
+		$post2 = $this->factory->post->create( array( 'post_title' => 'post2', 'post_category' => array( $cat2 ) ) );
+
+		// Assert that each category has one post.
+		$posts_in_cat1 = $this->get_posts_by_category( $cat1 );
+		$posts_in_cat2 = $this->get_posts_by_category( $cat2 );
+		$posts_in_common_cat = $this->get_posts_by_category( $common_cat );
+
+		$this->assertEquals( count( $posts_in_cat1 ), 1 );
+		$this->assertEquals( count( $posts_in_cat2 ), 1 );
+		$this->assertEquals( count( $posts_in_common_cat ), 1 );
+
+		// call our method.
+		$options = array(
+			'old_cat'   => $cat1,
+			'new_cat'   => $cat2,
+			'overwrite' => true,
+		);
+		$this->category_metabox->move( $options );
+
+		// Assert that category 1 (or) default category has no posts.
+		$posts_in_cat1 = $this->get_posts_by_category( $cat1 );
+		$this->assertEquals( count( $posts_in_cat1 ), 0 );
+
+		// Assert that common category has no posts.
+		$posts_in_common_cat = $this->get_posts_by_category( $common_cat );
+		$this->assertEquals( count( $posts_in_common_cat ), 0 );
+
+		// Assert that category 2 has two posts.
+		$posts_in_cat2 = $this->get_posts_by_category( $cat2 );
+		$this->assertEquals( count( $posts_in_cat2 ), 2 );
+	}
 }
