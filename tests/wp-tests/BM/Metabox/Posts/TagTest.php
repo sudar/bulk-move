86b34d01d35574d82280d281c859df8d8ec5f3e8
anandB000 <anandrajbalu00@gmail.com>
2017-12-29 23:53:19 +0530

    Add test for move tags without overwrite option
    see #39

--- tests/wp-tests/BM/Metabox/Posts/TagTest.php
+++ tests/wp-tests/BM/Metabox/Posts/TagTest.php
@@ -106,6 +106,46 @@ class TagTest extends BM_TestCase {
 	 * Test Moving tag without overwrite.
 	 */
 	public function test_move_posts_from_one_tag_to_another_without_overwrite() {
+        // Create two tags and a common tag.
+        $tag1 = $this->factory->tag->create( array( 'name' => 'tag1' ) );
+        $tag2 = $this->factory->tag->create( array( 'name' => 'tag2' ) );
+        $common_tag = $this->factory->tag->create( array( 'name' => 'common_tag' ) );
 
+        // Create one post in each tag.
+        // The first post will also have the common tag.
+        $post1 = $this->factory->post->create( array( 'post_title' => 'post1' ) );
+        wp_set_post_tags( $post1, array( 'tag1', 'common_tag' ) );
+
+        $post2 = $this->factory->post->create( array( 'post_title' => 'post2' ) );
+        wp_set_post_tags( $post2, 'tag2' );
+
+        // Assert that each tag has one post.
+        $posts_in_tag1 = $this->get_posts_by_tag( $tag1 );
+        $posts_in_tag2 = $this->get_posts_by_tag( $tag2 );
+        $posts_in_common_tag = $this->get_posts_by_tag( $common_tag );
+
+        $this->assertEquals( count( $posts_in_tag1 ), 1 );
+        $this->assertEquals( count( $posts_in_tag2 ), 1 );
+        $this->assertEquals( count( $posts_in_common_tag ), 1 );
+
+        // call our method.
+        $options = array(
+            'old_tag'   => $tag1,
+            'new_tag'   => $tag2,
+            'overwrite' => false,
+        );
+        $this->tag_metabox->move( $options );
+
+        // Assert that tag 1 has no posts.
+        $posts_in_tag1 = $this->get_posts_by_tag( $tag1 );
+        $this->assertEquals( count( $posts_in_tag1 ), 0 );
+
+        // Assert that common tag has one posts.
+        $posts_in_common_tag = $this->get_posts_by_tag( $common_tag );
+        $this->assertEquals( count( $posts_in_common_tag ), 1 );
+
+        // Assert that tag 2 has two posts.
+        $posts_in_tag2 = $this->get_posts_by_tag( $tag2 );
+        $this->assertEquals( count( $posts_in_tag2 ), 2 );
 	}
 }
