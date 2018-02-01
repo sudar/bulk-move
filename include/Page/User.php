26adb68311d0c6cbe29734107152aa00121df291
Maria Daniel Deepak <mariadanieldeepak@gmail.com>
2018-02-01 12:04:43 +0530

    Fix incorrect copy
    Fix review comment: https://github.com/sudar/bulk-move/pull/73#discussion_r164367977

--- include/Page/User.php
+++ include/Page/User.php
@@ -13,7 +13,7 @@ class BM_Page_User extends BM_Page_Base {
 		$this->slug            = 'bulk-move-users';
 		$this->page_title      = __( 'Bulk Move Users', 'bulk-move' );
 		$this->menu_title      = __( 'Bulk Move Users', 'bulk-move' );
-		$this->warning_message = __( 'WARNING: Posts moved once cannot be reverted. Use with caution.', 'bulk-move' );
+		$this->warning_message = __( 'WARNING: Users moved once cannot be reverted. Use with caution.', 'bulk-move' );
 		$this->capability      = 'edit_others_posts';
 	}
 
