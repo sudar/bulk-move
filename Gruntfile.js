52e39d8dc61aaf24e0bae4206dcc90e3c5408869
Sudar Muthu <sudar@sudarmuthu.com>
2018-02-19 21:56:26 +0530

    Copy grunt tasks from bulk delete

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,11 +9,111 @@ module.exports = function( grunt ) {
 	grunt.initConfig( {
 		pkg:    grunt.file.readJSON( 'package.json' ),
 
+		jshint: {
+			browser: {
+				files: {
+					src: [ 'assets/js/src/*.js' ]
+				},
+				options: {
+					jshintrc: '.jshintrc'
+				}
+			},
+			grunt: {
+				files:{
+					src: [ 'Gruntfile.js' ]
+				},
+				options: {
+					jshintrc: '.jshintrc'
+				}
+			}
+		},
+
+		concat: {
+			options: {
+				stripBanners: true,
+				banner: '/*! <%= pkg.title %> - v<%= pkg.version %> %>\n' +
+					' * <%= pkg.homepage %>\n' +
+					' * Copyright (c) <%= grunt.template.today("yyyy") %>;' +
+					' * Licensed GPLv2+' +
+					' */\n',
+				sourceMap: true
+			},
+			css: {
+				files: {
+					'assets/css/bulk-move.css': ['assets/css/src/**/*.css']
+				}
+			},
+			scripts: {
+				files: {
+					'assets/js/bulk-move.js': ['assets/js/src/**/*.js']
+				}
+			}
+		},
+
+		uglify: {
+			all: {
+				options: {
+					banner: '/*! <%= pkg.title %> - v<%= pkg.version %> \n' +
+						' * <%= pkg.homepage %>\n' +
+						' * Copyright (c) <%= grunt.template.today("yyyy") %>;' +
+						' * Licensed GPLv2+' +
+						' */\n',
+					mangle: {
+						reserved: ['jQuery', 'BULK_MOVE']
+					}
+				},
+				files: {
+					'assets/js/bulk-move.min.js': ['assets/js/bulk-move.js']
+				}
+			}
+		},
+
+		cssmin: {
+			minify: {
+				src: 'assets/css/bulk-move.css',
+				dest: 'assets/css/bulk-move.min.css'
+			}
+		},
+
+		watch:  {
+			scripts: {
+				files: ['assets/js/src/**/*.js'],
+				tasks: ['jshint:browser', 'concat', 'uglify'],
+				options: {
+					debounceDelay: 500
+				}
+			},
+			css: {
+				files: ['assets/css/src/**/*.css'],
+				tasks: ['concat', 'cssmin'],
+				options: {
+					debounceDelay: 500
+				}
+			},
+			build: {
+				files: ['**', '!dist/**'],
+				tasks: ['build'],
+				options: {
+					debounceDelay: 500
+				}
+			}
+		},
+
 		clean   : {
 			dist: ['dist/']
 		},
 
 		copy: {
+			select2: {
+				files: [{
+					src : 'node_modules/select2/dist/js/select2.min.js',
+					dest: 'assets/js/select2.min.js'
+				},
+				{
+					src : 'node_modules/select2/dist/css/select2.min.css',
+					dest: 'assets/css/select2.min.css'
+				}]
+			},
 			dist: {
 				files : [
 					{
@@ -22,6 +122,7 @@ module.exports = function( grunt ) {
 							'**',
 							'!dist/**',
 							'!AUTHORS.md',
+							'!docs/**',
 							'!assets-wp-repo/**',
 							'!code-coverage/**',
 							'!codeception.yml',
@@ -30,9 +131,7 @@ module.exports = function( grunt ) {
 							'!assets/js/src/**',
 							'!assets/css/src/**',
 							'!Gruntfile.js',
-							'!bower.json',
 							'!package.json',
-							'!package-lock.json',
 							'!composer.json',
 							'!composer.lock',
 							'!phpcs.xml',
@@ -48,18 +147,14 @@ module.exports = function( grunt ) {
 					}
 				]
 			}
-		},
-		watch: {
-			all: {
-				files: ['**', '!dist/**'],
-				tasks: ['build']
-			}
 		}
 	} );
 
 	require('time-grunt')(grunt);
 
-	grunt.registerTask('build', ['clean', 'copy:dist']);
+	grunt.registerTask('default', ['jshint:browser', 'concat', 'uglify', 'cssmin']);
+	grunt.registerTask('vendor', ['copy:select2']);
+	grunt.registerTask('build', [ 'default', 'vendor', 'clean', 'copy:dist']);
 
 	grunt.util.linefeed = '\n';
 };
