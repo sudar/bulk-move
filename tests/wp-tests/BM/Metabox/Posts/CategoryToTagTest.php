d35f8aa24daf659044781814800c705f261413c3
anandB000 <anandrajbalu00@gmail.com>
2018-03-01 00:54:17 +0530

    Remove 'test_move_posts_from_category_to_tag' function on CategoryToTagTest.php
    Fix #89

--- tests/wp-tests/BM/Metabox/Posts/CategoryToTagTest.php
+++ tests/wp-tests/BM/Metabox/Posts/CategoryToTagTest.php
@@ -17,48 +17,7 @@ class CategoryToTagTest extends WPCoreUnitTestCase {
 
 		$this->category_to_tag_metabox = new \BM_Metabox_Posts_CategoryToTag();
 	}
-
-	/**
-	 * Test basic case of moving category to tag.
-	 */
-	public function test_move_posts_from_category_to_tag() {
-		// Create one tag and category.
-		$tag = $this->factory->tag->create( array( 'name' => 'tag' ) );
-		$cat = $this->factory->category->create( array( 'name' => 'cat' ) );
-
-		// Create one post in tag and category.
-		$post_tag = $this->factory->post->create( array( 'post_title' => 'post_tag' ) );
-		wp_set_post_tags( $post_tag, 'tag' );
-
-		$post_cat = $this->factory->post->create( array(
-			'post_title'    => 'post_cat',
-			'post_category' => array( $cat )
-		) );
-
-		// Assert that tag has one post and category has one post.
-		$posts_in_tag = $this->get_posts_by_tag( $tag );
-		$posts_in_cat = $this->get_posts_by_category( $cat );
-
-		$this->assertEquals( 1, count( $posts_in_tag ) );
-		$this->assertEquals( 1, count( $posts_in_cat ) );
-
-		// call our method.
-		$options = array(
-			'cat'       => $cat,
-			'tag'       => $tag,
-			'overwrite' => true,
-		);
-		$this->category_to_tag_metabox->move( $options );
-
-		// Assert that tag 1 has two posts.
-		$posts_in_tag = $this->get_posts_by_tag( $tag );
-		$this->assertEquals( 2, count( $posts_in_tag ) );
-
-		// Assert that category has no posts.
-		$posts_in_cat = $this->get_posts_by_category( $cat );
-		$this->assertEquals( 0, count( $posts_in_cat ) );
-	}
-
+	
 	/**
 	 * Test case of moving category to tag without overwrite.
 	 */
